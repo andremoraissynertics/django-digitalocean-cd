83ac93d454a414b823fbe79257c38b8ba8514255
andremoraissynertics <138239118+andremoraissynertics@users.noreply.github.com>
2023-08-01 17:47:37 +0100

    Update wsgi.py

--- mysite/wsgi.py
+++ mysite/wsgi.py
@@ -8,9 +8,14 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
 """
 
 import os
+import sys
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+sys.path.append(BASE_DIR)
 
 from django.core.wsgi import get_wsgi_application
 
+os.environ['DJANGO_SETTINGS_MODULE'] = 'mysite.settings'
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
 
 application = get_wsgi_application()
