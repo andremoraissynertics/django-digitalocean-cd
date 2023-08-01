6567888d2a85ca28b9bec42c9565dd1377fc42c8
andremoraissynertics <138239118+andremoraissynertics@users.noreply.github.com>
2023-08-01 18:12:13 +0100

    Update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -131,7 +131,7 @@ USE_TZ = True
 
 STATIC_URL = "static/"
 STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'static_cdn')
-STATICFILES_DIRS = (os.path.join(str(os.path.join(BASE_DIR)), 'static'),)
+STATICFILES_DIRS = (os.path.join(str(os.path.join(BASE_DIR)), 'static_cdn'),)
 
 
 # Default primary key field type
